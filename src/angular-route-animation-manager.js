e7b59a1e2015e43acfaa4a482a4c4ab56d9529fb
Traxmaxx <github@netzok.net>
2014-01-16 23:25:46 +0100

    Fix TypeError: Cannot read property 'originalPath' of undefined

--- src/angular-route-animation-manager.js
+++ src/angular-route-animation-manager.js
@@ -5,7 +5,7 @@ app.run(['$rootScope', 'RouteAnimationManager', function($rootScope, RouteAnimat
   $rootScope.$on('$routeChangeStart', function(scope, next, current) {
     RouteAnimationManager.setAnimationClass(current, next );
   });
-  
+
   $rootScope.RouteAnimationManager = {
     animationClass : RouteAnimationManager.animationClass
   };
@@ -13,7 +13,7 @@ app.run(['$rootScope', 'RouteAnimationManager', function($rootScope, RouteAnimat
 
 
 app.provider('RouteAnimationManager', function()  {
-  
+
   //private vars
   var _defaultAnimation = '';
   var _animationClass = { name:''};
@@ -22,23 +22,28 @@ app.provider('RouteAnimationManager', function()  {
   this.setDefaultAnimation = function(animation) {
     _defaultAnimation = animation;
   };
-  
+
   //return factory instance
   this.$get = function() {
     return new RouteAnimationManager();
   };
-   
+
   //define factory instance
   function RouteAnimationManager() {
     this.setAnimationClass =  function(currentRoute, nextRoute) {
       if (!nextRoute || !nextRoute.originalPath || !nextRoute.data || !nextRoute.data.animationConf) {return undefined;}
-      
-      var conf = nextRoute.data.animationConf;
-      var name = currentRoute.originalPath.substring(1) || 'root'; //root refers to the '/' route
-    
+
+      var name, conf = nextRoute.data.animationConf;
+
+      if (currentRoute && currentRoute.originalPath) {
+        name = currentRoute.originalPath.substring(1);
+      } else {
+        name = 'root'; //root refers to the '/' route
+      }
+
       _animationClass.name = conf[name] ||  conf.default || _defaultAnimation;
     };
-    
+
     this.animationClass = _animationClass;
   }
 });
